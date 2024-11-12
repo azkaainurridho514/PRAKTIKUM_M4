a87bbef4b9c2afa7fe6e827a92d1d95616c0a044
asasas <asas>
2024-11-12 10:50:42 +0700

    praktikum 1

--- app/src/main/java/com/example/praktikum_m4_bp3/MainActivity.java
+++ app/src/main/java/com/example/praktikum_m4_bp3/MainActivity.java
@@ -1,15 +1,26 @@
 package com.example.praktikum_m4_bp3;
 
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button hitung;
+    private EditText tinggi;
+    private EditText panjang;
+    private EditText lebar;
+    private TextView result;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +31,42 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+         hitung = findViewById(R.id.hitung);
+         tinggi = findViewById(R.id.tinggi);
+         panjang = findViewById(R.id.panjang);
+         lebar = findViewById(R.id.lebar);
+         result = findViewById(R.id.hasil);
+        hitung.setOnClickListener((View.OnClickListener) this);
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState, @NonNull PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+    }
+
+    @Override
+    public void onClick(View v) {
+     if(v.getId() == R.id.hitung){
+         String inputPanjang = panjang.getText().toString().trim();
+         String inputLebar = lebar.getText().toString().trim();
+         String inputTinggi = tinggi.getText().toString().trim();
+         boolean isEmptyFields = false;
+         if(TextUtils.isEmpty(inputPanjang)){
+             isEmptyFields = true;
+             panjang.setError("Field panjang harus di isi");
+         }
+         if(TextUtils.isEmpty(inputLebar)){
+             isEmptyFields = true;
+             lebar.setError("Field lebar harus di isi");
+         }
+         if(TextUtils.isEmpty(inputTinggi)){
+             isEmptyFields = true;
+             tinggi.setError("Field panjang tinggi di isi");
+         }
+         if(!isEmptyFields){
+             double volume = Double.valueOf(inputPanjang) * Double.valueOf(inputLebar) * Double.valueOf(inputTinggi);
+             result.setText(String.valueOf(volume));
+         }
+     }
     }
 }
